def23a467a2f5a8e0ad54307d16f4ee53d2a52a0
elina <aelin6277@gmail.com>
2023-01-28 21:50:56 +0100

    Name of the pizza is a button, you see the ingredients hen you press the chosen pizza

--- PizzaMenu/ContentView.swift
+++ PizzaMenu/ContentView.swift
@@ -4,18 +4,37 @@
 //
 //  Created by Elin A on 2023-01-28.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var title: String = "Pizza Menu, press a pizza "
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text(title)
+            Button("Margherita") {
+                self.title = "Tomatsås, ost"
+                
+            }
+            
+            Button(action: {
+                    self.title = "Tomatsås, ost, champinjoner"
+            }, label: {
+                Text("Funghi")
+            })
+
+            Button(action: {
+                    self.title = "Tomatsås, ost, champinjoner, skinka"
+            }, label: {
+                Text("Capricciosa")
+            })
+            
+            Button(action: {
+                    self.title = "Press the name of the pizza to see the ingredients"
+            }, label: {
+                Text("Refresh")
+            })
         }
-        .padding()
     }
 }
 
