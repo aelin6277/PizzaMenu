60531b85ef2bb78aab6c3c5473c4e62d981af53a
elina <aelin6277@gmail.com>
2023-01-28 21:27:39 +0100

    Initial Commit

---
+++ PizzaMenu/PizzaMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaMenuApp.swift
+//  PizzaMenu
+//
+//  Created by Elin A on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct PizzaMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
